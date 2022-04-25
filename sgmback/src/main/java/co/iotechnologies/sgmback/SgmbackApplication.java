fda0a22030d8b893272a50df2fd7f87ce61cc85b
Catherine Escobar Trujillo <bettelgeuxe@hotmail.com>
2022-04-25 10:15:11 -0500

    back spring
    Inicialización SGM

---
+++ sgmback/src/main/java/co/iotechnologies/sgmback/SgmbackApplication.java
@@ -0,0 +1,13 @@
+package co.iotechnologies.sgmback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgmbackApplication.class, args);
+	}
+
+}
---
+++ sgmback/src/test/java/co/iotechnologies/sgmback/SgmbackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.iotechnologies.sgmback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgmbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
